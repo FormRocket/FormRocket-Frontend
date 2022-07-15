d46fe8fd5e45f68e662ed8b13a30b17b657ce843
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-15 08:17:58 +0000

    Commit

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,26 @@
 <template>
     <template v-if="session.user">
-        <Navbar :session="session" :showLogout="true" />
-        <h1>Account: {{session.user.name}}</h1>
-        <h1>ID: {{session.user.id}}</h1>
-        <h1>Subscription: {{plans[session.user.plan]}}</h1>
-        <img style="width: 50px; height: 50px" :src="session.user.avatar" />
+        <Navbar :session="session" :showLogout="true" :plan="plans[session.user.plan]" />
+        <div class="p-4">
+            <h1 class="text-6xl mb-3">Hey, <span style="display:initial !important;"
+                    class="text-lg-gradient">{{ session.user.name }}</span>!</h1>
+            <p class="text-lg mb-5">Manage your forms.</p>
+
+            <div class="card shadow rounded-md p-4 max-w-sm w-full cursor-pointer bg-[#2e2e2f] hover:bg-[#212122]">
+                <h1 class="text-2xl mb-3">Cool form</h1>
+                <p class="text-lg">Created yesterday.</p> <!-- use the time library from the old version -->
+            </div>
+        </div>
     </template>
     <template v-else>
-        <Navbar :session="null" :showLogin="true" />
+        <Navbar :session="null" :showLogin="true" :plan="null" />
+        <div class="p-4">
+            <center>
+                <h1 class="text-6xl font-bold m-3">Introducing <span class="text-lg-gradient">FormRocket3.</span></h1>
+                <p class="text-lg">Create powerful HTML forms, store responses, and send to your favorite apps.</p>
+            </center>
+
+        </div>
     </template>
 </template>
 
