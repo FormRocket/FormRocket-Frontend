fcc1dba88dd3b7de2b63d11d5fb4b9ff837d3ba6
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-18 14:54:57 -0700

    Create NewForm.vue

---
+++ src/NewForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="!session.user">Your not logged in bozo</div>
+
+    <div v-else>
+create form
+</div>
+</template>
+<script setup>
+import { session } from '@/services/auth.js'
+import { useRoute } from 'vue-router';
+const route = useRoute();
+</script>
