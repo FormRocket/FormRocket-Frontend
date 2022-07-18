879ecbd786a18989b7f7cdee86e8a6a8ddc2f9c1
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-18 01:06:07 +0000

    ball sacks

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,7 +6,7 @@
                     class="text-lg-gradient">{{ session.user.name }}</span>!</h1>
             <p class="text-lg mb-5">Manage your forms.</p>
 
-            <div class="card shadow rounded-xl p-4 max-w-sm w-full cursor-pointer bg-[#111010] hover:bg-[#212122]">
+            <div class="card shadow rounded-xl p-4 max-w-sm w-full cursor-pointer bg-[#111111]">
                 <h1 class="text-2xl mb-3">Cool form</h1>
                 <p class="text-lg">Created yesterday.</p> <!-- use the time library from the old version -->
             </div>
@@ -15,17 +15,63 @@
     <template v-else>
         <Navbar :session="null" :showLogin="true" />
         <div class="p-10 appContent">
-            <center>
-                <h1 class="text-6xl font-bold m-3">Introducing <span class="text-lg-gradient">FormRocket3.</span></h1>
-                <p class="text-lg">Create powerful HTML forms, store responses, and send to your favorite apps.</p>
-            </center>
+<div class="h-auto">
+    <div class="px-10 py-24 mx-auto max-w-7xl">
+        <div class="w-full mx-auto text-left md:text-center">
+            <h1 class="mb-6 text-5xl font-extrabold leading-none max-w-5xl mx-auto tracking-normal sm:text-6xl md:text-6xl lg:text-7xl md:tracking-tight"> Introducing <span class="w-full text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 lg:inline">the all new</span> <br class="lg:block hidden"> FormRocket3. </h1>
+            <p class="px-0 mb-6 text-lg text-gray-300 md:text-xl lg:px-24"> A powerful backend for all your HTML forms. Recieve submissions &amp; send to your favorite destinations. </p>
+            <button @click="login" class="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-pink-800">GET STARTED</button>
+            
+        </div>
+    </div>
+</div>
+
+
+
+<section class="w-full pt-7 pb-7 md:pt-20 md:pb-24">
+    <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+
+        <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+            <img src="https://www.formrocket.me/files/svg/MeditatingDoodle.svg" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 ">
+        </div>
+
+        <div class="box-border order-first w-full text-white border-solid md:w-1/2 md:pl-10 md:order-none">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Free storage for your submissions
+            </h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-gray-300 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+                Store 500 responses for free and send them to your favorite destinations.    
+            </p>
+        </div>
+    </div>
+    <div class="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
+
+        <div class="box-border w-full text-white border-solid md:w-1/2 md:pl-6 xl:pl-32">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Instant plug and play
+            </h2>
+            <p class="pt-4 pb-8 m-0 leading-7 text-gray-300 border-0 border-gray-300 sm:pr-10 lg:text-lg">
+                Create forms in seconds &amp; recieve submissions instantly.
+            </p>
+            
+        </div>
+
+        <div class="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
+            <img src="https://www.formrocket.me/files/svg/ReadingSideDoodle.svg" class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32">
+        </div>
+    </div>
+</section>
+
+<Footer />
+
         </div>
     </template>
 </template>
 
 <script setup>
-import { session } from '@/services/auth.js'
+import { session, login, logout } from '@/services/auth.js'
 import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 
 const plans = {
     0: "Basic",
