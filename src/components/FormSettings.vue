5d4e29ccc078a75de68531e4297c2e70eaa960f4
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-20 05:35:28 +0000

    ;

--- src/components/FormSettings.vue
+++ src/components/FormSettings.vue
@@ -20,12 +20,103 @@
                 <p>View and manage this form&apos;s responses.</p>
 
                 <div class="bg-[#121212] mx-[5%] my-[3%] p-4 rounded-lg">
-                    <h1 class="text-4xl mb-4">Response #1</h1>
+                    <h1 class="text-4xl mb-4">Response #1 <Icon icon="Paper" bulk size="35px" /></h1>
                     <div class="md:flex md:mb-5 mb-8">
                           <button class="settingsBarItem mr-3 px-4 py-1 bg-[#151515] hover:bg-[#151515] rounded-lg"><b>RESPONSE DATA</b></button>
                           <button class="settingsBarItem mr-3 px-4 py-1 hover:bg-[#151515] rounded-lg">SENDER DATA</button>
-                          <p class="display-block"><b>name</b></p>: <code>"ar"</code>
+
                      </div>
+                     
+                     <div class="p-4 bg-[#151515] rounded-lg">
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>name</b>: <code>"ar"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>email</b>: <code>"test@example.com"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="bg-[#121212] mx-[5%] my-[3%] p-4 rounded-lg">
+                    <h1 class="text-4xl mb-4">Response #2 <Icon icon="Paper" bulk size="35px" /></h1>
+                    <div class="md:flex md:mb-5 mb-8">
+                          <button class="settingsBarItem mr-3 px-4 py-1 bg-[#151515] hover:bg-[#151515] rounded-lg"><b>RESPONSE DATA</b></button>
+                          <button class="settingsBarItem mr-3 px-4 py-1 hover:bg-[#151515] rounded-lg">SENDER DATA</button>
+
+                     </div>
+                     
+                     <div class="p-4 bg-[#151515] rounded-lg">
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>name</b>: <code>"bob"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>email</b>: <code>"bob@example.com"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="bg-[#121212] mx-[5%] my-[3%] p-4 rounded-lg">
+                    <h1 class="text-4xl mb-4">Response #3 <Icon icon="Paper" bulk size="35px" /></h1>
+                    <div class="md:flex md:mb-5 mb-8">
+                          <button class="settingsBarItem mr-3 px-4 py-1 bg-[#151515] hover:bg-[#151515] rounded-lg"><b>RESPONSE DATA</b></button>
+                          <button class="settingsBarItem mr-3 px-4 py-1 hover:bg-[#151515] rounded-lg">SENDER DATA</button>
+
+                     </div>
+                     
+                     <div class="p-4 bg-[#151515] rounded-lg">
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>name</b>: <code>"linda"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>email</b>: <code>"linda@example.com"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="bg-[#121212] mx-[5%] my-[3%] p-4 rounded-lg">
+                    <h1 class="text-4xl mb-4">Response #4 <Icon icon="Paper" bulk size="35px" /></h1>
+                    <div class="md:flex md:mb-5 mb-8">
+                          <button class="settingsBarItem mr-3 px-4 py-1 bg-[#151515] hover:bg-[#151515] rounded-lg"><b>RESPONSE DATA</b></button>
+                          <button class="settingsBarItem mr-3 px-4 py-1 hover:bg-[#151515] rounded-lg">SENDER DATA</button>
+
+                     </div>
+                     
+                     <div class="p-4 bg-[#151515] rounded-lg">
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>name</b>: <code>"ben"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>email</b>: <code>"ben@example.com"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="bg-[#121212] mx-[5%] my-[3%] p-4 rounded-lg">
+                    <h1 class="text-4xl mb-4">Response #5 <Icon icon="Paper" bulk size="35px" /></h1>
+                    <div class="md:flex md:mb-5 mb-8">
+                          <button class="settingsBarItem mr-3 px-4 py-1 bg-[#151515] hover:bg-[#151515] rounded-lg"><b>RESPONSE DATA</b></button>
+                          <button class="settingsBarItem mr-3 px-4 py-1 hover:bg-[#151515] rounded-lg">SENDER DATA</button>
+
+                     </div>
+                     
+                     <div class="p-4 bg-[#151515] rounded-lg">
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>name</b>: <code>"alice"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                     <div class="md:flex mb-2">
+                        <p class="md:w-1/2"><b>email</b>: <code>"alice@example.com"</code></p>
+                        <p class="md:ml-9 md:w-1/2"><b>Type</b>: <code>String</code></p>
+                    </div>
+                    </div>
                 </div>
                 
                      
