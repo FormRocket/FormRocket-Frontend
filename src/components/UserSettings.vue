0cc7e5e8fc38f81c16bdeca2381d10355d4598cf
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-22 04:11:40 +0000

    .

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -17,15 +17,23 @@
                 
                 <h1 class="text-4xl mt-8 mb-2">Username <Icon icon="User2" bulk size="35px" /></h1>
                 <p>This field cannot be modified.</p>
-                <input disabled :value="session.user.name" class="text-white p-1 mt-4 px-3 rounded-lg" />
+                <input disabled :value="session.user.name" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
                 <h1 class="text-4xl mt-8 mb-2">User ID <Icon icon="Info-Square" bulk size="35px" /></h1>
                 <p>This field cannot be modified.</p>
-                <input disabled :value="session.user.id" class="text-white p-1 mt-4 px-3 rounded-lg" />
+                <input disabled :value="session.user.id" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
-                <h1 class="text-4xl mt-8 mb-2">User Token <Icon icon="Password" bulk size="35px" /></h1>
+                <h1 class="text-4xl mt-8 mb-2">Plan <Icon icon="Bag-2" bulk size="35px" /></h1>
+                <p>Your current FormRocket plan. This can be managed from <a class="text-blue-600 cursor-pointer hover:text-blue-500" @click="$router.push('/subscriptions')">here</a>.</p>
+                <input disabled :value="plans[session.user.plan]" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+
+                <h1 class="text-4xl mt-8 mb-2">Remove My Data <Icon icon="Delete" bulk size="35px" /></h1>
+                <p>Request complete removal of your data by <a class="text-blue-600 cursor-pointer hover:text-blue-500" onclick="window.location.href = 'emailto:formrocket@protonmail.com'">sending us an email</a>.</p>
+                
+                <!--<h1 class="text-4xl mt-8 mb-2">User Token <Icon icon="Password" bulk size="35px" /></h1>
                 <p>This is used as an authorization token for all FormRocket requests.<br /><b>Never share with anyone.</b></p>
                 <input disabled :value="session.token" class="text-white p-1 mt-4 px-3 rounded-lg" />
+                -->
 
                 
                 </div>
@@ -40,4 +48,10 @@ import ShowError from '@/components/ShowError.vue';
 import Navbar from '@/components/Navbar.vue';
 import Icon from '@/iconly/iconly.vue';
 const route = useRoute();
+
+const plans = {
+    0: "Basic",
+    1: "Premium",
+    2: "Business"
+}
 </script>
