74ffeaad5402c264145f7c985e8b9b16f9112da5
ar <74219764+AR-Student824@users.noreply.github.com>
2022-07-24 01:54:10 +0000

    make form builder

--- src/components/NewForm.vue
+++ src/components/NewForm.vue
@@ -17,7 +17,7 @@
                 <div v-if="atState == 'formName'">
                 <h1 class="text-3xl mt-8 mb-2">What would you like your form to be called?</h1>
                 <p>Set a name for your new form.</p>
-                <input v-bind:value="formObj.name" v-on:input="formObj.name = $event.target.value" placeholder="Cool form" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                <input v-bind:value="formObj.formName" v-on:input="formObj.formName = $event.target.value" placeholder="Cool form" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
                 <div class="block pt-[20px]">
                 <button disabled style="opacity: 50%" class="cursor-not-allowed sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none">&lt; BACK</button> 
@@ -42,20 +42,53 @@
                 <p>Setup integrations and get notified when a new response is recieved.</p>
 
                 <h1 class="text-2xl mt-4">Discord</h1>
-                <input placeholder="Discord Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                <input v-bind:value="formObj.discordWebhook" v-on:input="formObj.discordWebhook = $event.target.value" placeholder="Discord Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
                 <h1 class="text-2xl mt-4">Guilded</h1>
-                <input placeholder="Guilded Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                <input v-bind:value="formObj.guildedWebhook" v-on:input="formObj.guildedWebhook = $event.target.value" placeholder="Guilded Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
                 <h1 class="text-2xl mt-4">Slack</h1>
-                <input placeholder="Slack Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                <input v-bind:value="formObj.slackWebhook" v-on:input="formObj.slackWebhook = $event.target.value" placeholder="Slack Webhook URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
 
-                <h1 class="text-2xl mt-4">Custom Server</h1>
-                <input placeholder="Custom Server URL" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                <h1 class="text-2xl mt-4">Custom Server (Premium)</h1>
+                <input v-bind:value="formObj.customWebhook" v-on:input="formObj.customWebhook = $event.target.value" :placeholder="session.user.plan >= 1 ? 'Custom Server URL' : 'Premium Feature'" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" :disabled="session.user.plan >= 1 ? false : true"  />
                 
                 <div class="block pt-[20px]">
                 <button class="introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState('blockedIps')">&lt; BACK</button> 
-                <button class="ml-2 introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState(null)">NEXT &gt;</button>
+                <button class="ml-2 introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState('thankYouPage')">NEXT &gt;</button>
+                </div>
+                </div>
+
+                <div v-if="atState == 'thankYouPage'">
+                <h1 class="text-3xl mt-8 mb-2">Would you like to customize your form&apos;s thank you page?</h1>
+                <p>Personalize your thank you page or set your own.</p>
+
+                <h1 class="text-2xl mt-4">Title Text</h1>
+                <input v-bind:value="formObj.titleText" v-on:input="formObj.titleText = $event.target.value" placeholder="Thank you!" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+
+                <h1 class="text-2xl mt-4">Success Message</h1>
+                <input v-bind:value="formObj.successMessage" v-on:input="formObj.successMessage = $event.target.value" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+
+                <h1 class="text-2xl mt-4">Back Text</h1>
+                <input v-bind:value="formObj.backText" v-on:input="formObj.backText = $event.target.value" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+
+                <h1 class="text-2xl mt-4 mb-3">Back URL</h1>
+                <p>Defaults to the browsers default back action.</p>
+                <input placeholder="Back URL" v-bind:value="formObj.backUrl" v-on:input="formObj.backUrl = $event.target.value" class="cursor-text text-gray-300 p-1 mt-4 px-3 rounded-lg" />
+                
+                <div class="block pt-[20px]">
+                <button class="introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState('integrations')">&lt; BACK</button> 
+                <button class="ml-2 introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState('createForm')">NEXT &gt;</button>
+                </div>
+                </div>
+
+                <div v-if="atState == 'createForm'">
+                <h1 class="text-3xl mt-8 mb-2">Create your form</h1>
+                <p>Lets finish building <b>{{formObj.formName || "your new form"}}</b>!</p>
+                <div class="block pt-[20px]">
+                <button class="introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="changeState('thankYouPage')">&lt; BACK</button>
+                    <button class="ml-2 introductionButton sm:display-block text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-[#222222] focus:outline-none" @click="createForm">CREATE FORM &gt;</button> 
+                
                 </div>
                 </div>
 
@@ -88,66 +121,103 @@ function changeState(state) {
 }
 
 const formObj = {
-    name: null,
+    formName: null,
     discordWebhook: null,
     customWebhook: null,
     guildedWebhook: null,
     slackWebhook: null,
     blockedIps: null,
-    successMessage: null,
-    backText: null,
+    successMessage: "This form has been submitted successfully! You may close this window now.",
+    backText: "Back",
     backUrl: null,
-    titleText: null,
+    titleText: "Thank you!",
     discordWebhookUsername: null,
     discordWebhookAvatarUrl: null
 }
 
 function validateForm() {
-    if (formObj.name == null) {
+    if (formObj.formName == null) {
         return {
             error: true,
-            message: "Please enter a name for your form."
+            message: "Please enter a name for your form.",
+            state: "formName"
         }
     }
 
-    if (!formObj.discordWebhook.startsWith("https://discordapp.com/api/webhooks/")) {
+    if (formObj.discordWebhook && !formObj.discordWebhook.startsWith("https://discordapp.com/api/webhooks/")) {
         return {
             error: true,
-            message: "Please enter a valid Discord Webhook URL."
+            message: "Please enter a valid Discord Webhook URL.",
+            state: "integrations"
         }
     }
 
-    if (!formObj.customWebhook.startsWith("https://")) {
+    if (formObj.customWebhook && !formObj.customWebhook.startsWith("https://")) {
         return {
             error: true,
-            message: "Please enter a valid Custom Webhook URL."
+            message: "Please enter a valid Custom Webhook URL.",
+            state: "integrations"
         }
     }
 
-    if (!formObj.guildedWebhook.startsWith("https://media.guilded.gg/")) {
+    if (formObj.guildedWebhook && !formObj.guildedWebhook.startsWith("https://media.guilded.gg/")) {
         return {
             error: true,
-            message: "Please enter a valid Guilded Webhook URL."
+            message: "Please enter a valid Guilded Webhook URL.",
+            state: "integrations"
         }
     }
 
-    if (!formObj.slackWebhook.startsWith("https://hooks.slack.com/")) {
+    if (formObj.slackWebhook && !formObj.slackWebhook.startsWith("https://hooks.slack.com/")) {
         return {
             error: true,
-            message: "Please enter a valid Slack Webhook URL."
+            message: "Please enter a valid Slack Webhook URL.",
+            state: "integrations"
         }
     }
 
-    if (!formObj.backUrl.startsWith("https://")) {
+    if (formObj.backUrl && !formObj.backUrl.startsWith("https://")) {
         return {
             error: true,
-            message: "Please enter a valid Back URL."
+            message: "Please enter a valid Back URL.",
+            state: "thankYouPage"
         }
     }
 
     return {
         error: false
     }
+}
+
+function createForm() {
+    const validation = validateForm()
+    if (validation.error) {
+        changeState(validation.state)
+        swal("Oops!", validation.message, "error")
+        return
+    }
+
+    var url = "https://api.formrocket.me/api/forms/create";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("authorization", session.value.token);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+         if (xhr.status === 200) {
+            window.location.href = '/settings/forms/' + JSON.parse(xhr.responseText).data.formId
+         } else {
+              swal("Oops!", "Error creating form.")
+         }
+   }};
+
+var data = JSON.stringify(formObj);
+
+xhr.send(data);
+
 }
 
 // debug
